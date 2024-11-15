053a8d6ed53ee5568da35bf0297b48c0a5e37021
Yuhri <matthewyuh246@gmail.com>
2024-11-15 19:05:31 +0900

    init

---
+++ server/src/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/matthewyuh246/websocket/src/domain"
+	"github.com/matthewyuh246/websocket/src/handlers"
+)
+
+func main() {
+	hub := domain.NewHub()
+	go hub.RunLoop()
+
+	http.HandleFunc("/ws", handlers.NewWebsocketHandler(hub).Handle)
+
+	port := "8080"
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil); err != nil {
+		log.Panicln("Serve Error:", err)
+	}
+}
